7534acfcb9aee72288985000f1a3e5033718c714
James Hines <jameshines10@gmail.com>
2015-06-14 13:25:14 -0400

    Adds a simple unit test for the hello module

--- hello/hello.js
+++ hello/hello.js
@@ -1,3 +1,6 @@
 /**
  * Created by james on 6/14/15.
  */
+module.exports = function(str) {
+    return 'Hello ' + str + '!';
+}
--- hello/hello.spec.js
+++ hello/hello.spec.js
@@ -1,3 +1,11 @@
 /**
  * Created by james on 6/14/15.
  */
+var hello = require('./hello');
+var assert = require('assert');
+
+describe('hello', function(){
+   it('will say "Hello James!"', function(){
+       assert(hello('James') === 'Hello James!');
+   });
+});
